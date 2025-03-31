8d0a781e13fe16be12d55487b524ce96a3100f55
joasmarquesoliveira <joas.oliveira@germinare.org.br>
2025-03-31 10:44:06 -0300

    Aplicando cors no arquivo index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,8 +14,9 @@ const port = process.env.PORT;
 const express = require("express")
 
 const app = express();
+const cors = require('cors')
 
-app.use(express.json());
+app.use(cors());
 
 //====================================================ROTAS==========================================================//
 app.get("/", (req, res) =>{
